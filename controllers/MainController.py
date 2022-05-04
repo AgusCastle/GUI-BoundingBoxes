f066c7aae4e9186f13d98d612a20eb74e9d4fbf1
Agus Castillo <agu13xd@hotmail.com>
2022-05-04 00:58:03 -0500

    Update Utils, Main

--- controllers/MainController.py
+++ controllers/MainController.py
@@ -1,3 +1,4 @@
+from logging import root
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import QPixmap, QImage
 from utils.opencv import *
@@ -17,7 +18,12 @@ class MainViewController(QtWidgets.QMainWindow):
 
         self.ui.bttn_selectimg.clicked.connect(lambda: self.abrirFotografia())
         self.ui.bttn_boxes.clicked.connect(lambda: self.agregarBoxesImagenListView())
+        self.ui.listView.doubleClicked.connect(self.onCliked)
         self.show()
+
+    def onCliked(self, item):
+        index = self.ui.listView.currentRow()
+        print(self.imgdata['boxes'][index])
 
     def abrirFotografia(self):
         filename =  QtWidgets.QFileDialog.getOpenFileName(None, 'Buscar Imagen', '.', 'Image Files (*.png *.jpg *.jpeg *.bmp)')
@@ -30,10 +36,24 @@ class MainViewController(QtWidgets.QMainWindow):
             self.ui.lbl_imagen.setPixmap(pix)
     
     def agregarBoxesImagenListView(self):
-        entradas = ["Uno", "Dos", "Tres"]
-        self.ui.listView.addItems(entradas)
+        # Obviamente esta harcodeado pero con la funcion que esta se 
+        # supone que con esto ya debe de quedar
+        lista_boxes = list()
+        lista_labels = list()
 
-        imagen_cv = setBoxesToImage(self.ruta_imagen)
+        lista_boxes.append([360, 146, 471, 262])
+        lista_labels.append('Box 1')
+        lista_boxes.append([38, 66, 109, 145])
+        lista_labels.append('Box 2')
+        lista_boxes.append([649, 14, 715, 86])
+        lista_labels.append('Box 3')
+        lista_boxes.append([221, 54, 291, 124])
+        lista_labels.append('Box 4')
+
+        self.imgdata = {'name': 'nombreArchivo', 'boxes': lista_boxes, 'labels': lista_labels}
+        self.ui.listView.addItems(self.imgdata['labels'])
+
+        imagen_cv = setBoxesToImage(self.ruta_imagen, self.imgdata)
         imagen = QImage(imagen_cv.data, imagen_cv.shape[1], imagen_cv.shape[0], QImage.Format_RGB888).rgbSwapped()
         pix = QPixmap.fromImage(imagen).scaled(700, 700, QtCore.Qt.KeepAspectRatio)
         self.ui.lbl_imagen.setPixmap(pix)
--- utils/opencv.py
+++ utils/opencv.py
@@ -1,10 +1,20 @@
 import cv2
+import numpy as np
 
-def setBoxesToImage(path, boxes=[408,135,249,331]):
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+size_font = 1
+grosor = 2
+
+def setBoxesToImage(path, boxes={}):
+
     image = cv2.imread(path)
+    for box, label in zip(boxes['boxes'], boxes['labels']):
+        color = list(np.random.choice(range(256), size=3))
+        for x, _ in enumerate(color):
+            color[x] = float(color[x])
 
-    image = cv2.rectangle(image, (boxes[3], boxes[2]), (boxes[1], boxes[0]), (255, 0, 0), 2)
-
+        image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), color, 2)
+        image = cv2.putText(image, label, (box[0], box[3] + 18), font, size_font, color, grosor)
     return image
 
 
